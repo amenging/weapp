8fa552b9a79dbdffa96e10776eef10b9a8b9fbe3
ameng <1508235075@qq.com>
2018-09-10 11:49:46 +0800

    瀑布流

---
+++ components/masonry/item.js
@@ -0,0 +1,21 @@
+// sources/component/masonry-item/item.js
+Component({
+  properties: {
+    data: {
+      type: Object
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
---
+++ pages/masonry/masonry.js
@@ -0,0 +1,91 @@
+// pages/masonry/masonry.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        img: 'http://wx4.sinaimg.cn/large/007coAkZgy1fte26nirtwj30ku112q4e.jpg',
+        text: 'xxxxxxxxxxxxxx'
+      },
+      {
+        img: 'http://wx3.sinaimg.cn/large/007coAkZgy1ftg5qk0u5uj301e01jdfl.jpg',
+        text: 'xxxxxxxxxxxxxx'
+      },
+      {
+        img: 'http://wx4.sinaimg.cn/large/007coAkZgy1fui7q32we5j30ku1120xb.jpg',
+        text: 'xxxxxxxxxxxxxx'
+      },
+      {
+        img: 'http://wx4.sinaimg.cn/large/007coAkZgy1fuia60jg8uj30ku11241q.jpg',
+        text: 'xxxxxxxxxxxxxx'
+      },
+      {
+        img: 'http://wx2.sinaimg.cn/large/007coAkZgy1ftczpiz8prg305000xa9t.gif',
+        text: 'xxxxxxxxxxxxxx'
+      },
+      {
+        img: '/components/imgs/delete.png',
+        text: 'xxxxxxxxxxxxxx'
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
